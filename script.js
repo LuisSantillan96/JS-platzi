636b159242276d2f54bab0c059e7a664744b72dc
LuisSantillan96 <luistecisc@gmail.com>
2022-09-08 13:26:01 -0500

    Hola Mundo

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 console.log('Hola Mundo');
-
-//LLamarlo por tipo de etiqueta
-const h1 = document.querySelector('h1');
-
-
-h1.addEventListener('click', function(){
-    h1.classList.toggle('Azul');
-})
